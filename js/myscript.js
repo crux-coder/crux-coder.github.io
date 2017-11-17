2136ae62f46ec8520f5109f9a201a05b7323078d
Jasmin Mustafic <mustaficjasmin7@gmail.com>
2017-11-17 20:34:10 +0100

    updated portfolio

--- js/myscript.js
+++ js/myscript.js
@@ -2,9 +2,14 @@ var helloWorldSpan = document.getElementById("hello-world-text");
 var helloWorldLetters = ["H","e","l","l","o"," ","W","o","r","l","d"];
 var i = 0;
 
-(function loop() {
+var loop =  function() {
     helloWorldSpan.innerHTML += helloWorldLetters[i];
     if (++i < helloWorldLetters.length) {
         setTimeout(loop, Math.random()*500);
     }
-})();
+};
+
+window.onload = loop;
+
+
+
